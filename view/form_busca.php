52624971aaacd26e5539e25f86f18d0d295dd6b4
Davi - Designer Gráfico <ddavimatiasilva@gmail.com>
2026-05-13 21:34:26 -0300

    Delete view directory

--- view/form_busca.php
+++
@@ -1,6 +0,0 @@
-<?php require_once __DIR__ . '/nav.php'; ?>
-
-<form method="get" action="index.php">
-  <input type="hidden" name="buscar" value="1">
-  <button type="submit">Buscar</button>
-</form>
--- view/nav.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" href="view/style.css">
-<div class="nav">
-  <a href="index.php">Busca</a>
-  <a href="veiculos.php">Veículos</a>
-  <a href="clientes.php">Clientes</a>
-  <a href="reservas.php">Reservas</a>
-</div>
-<hr>
